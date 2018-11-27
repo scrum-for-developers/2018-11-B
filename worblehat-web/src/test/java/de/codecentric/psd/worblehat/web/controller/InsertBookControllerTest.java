f56dd26d50484d6a7d3195b0c2eb61c64a334bb0
jegom <jennifer.gommans@codecentric.de>
2018-11-27 11:22:21 +0100

    Test for edited description field.

--- worblehat-web/src/test/java/de/codecentric/psd/worblehat/web/controller/InsertBookControllerTest.java
+++ worblehat-web/src/test/java/de/codecentric/psd/worblehat/web/controller/InsertBookControllerTest.java
@@ -33,7 +33,9 @@ public class InsertBookControllerTest {
 
     private BindingResult bindingResult;
 
-    private static final Book TEST_BOOK = new Book("title", "author", "edition", "isbn", "description", 2016);
+    private static final Book TEST_BOOK = new Book("title", "author", "edition", "isbn", null, 2016);
+
+    private static final Book TEST_BOOK_WITH_DESCRIPTION = new Book("title", "author", "edition", "isbn", "description", 2016);
 
     @Before
     public void setUp() {
@@ -85,9 +87,27 @@ public class InsertBookControllerTest {
         assertThat(navigateTo, is("redirect:bookList"));
     }
 
+    @Test
+    public void shouldCreateBookWithoutDescriptionAndNavigateToBookList(){
+        setupFormDataWithDescription();
+        when(bookService.bookExists(TEST_BOOK_WITH_DESCRIPTION.getIsbn())).thenReturn(false);
+        when(bookService.createBook(any(), any(), any(), any(), any(), anyInt())).thenReturn(Optional.of(TEST_BOOK_WITH_DESCRIPTION));
+
+        String navigateTo = insertBookController.processSubmit(bookDataFormData, bindingResult);
+
+        verifyBookIsCreatedWithDescription();
+        assertThat(navigateTo, is("redirect:bookList"));
+    }
+
     private void verifyBookIsCreated() {
         verify(bookService).createBook(TEST_BOOK.getTitle(), TEST_BOOK.getAuthor(),
                 TEST_BOOK.getEdition(), TEST_BOOK.getIsbn(), TEST_BOOK.getDescription(), TEST_BOOK.getYearOfPublication());
+    }
+
+    private void verifyBookIsCreatedWithDescription(){
+        verify(bookService).createBook(TEST_BOOK_WITH_DESCRIPTION.getTitle(), TEST_BOOK_WITH_DESCRIPTION.getAuthor(),
+                TEST_BOOK_WITH_DESCRIPTION.getEdition(), TEST_BOOK_WITH_DESCRIPTION.getIsbn(),
+                TEST_BOOK_WITH_DESCRIPTION.getDescription(), TEST_BOOK_WITH_DESCRIPTION.getYearOfPublication());
     }
 
     private void setupFormData() {
@@ -96,5 +116,14 @@ public class InsertBookControllerTest {
         bookDataFormData.setEdition(TEST_BOOK.getEdition());
         bookDataFormData.setIsbn(TEST_BOOK.getIsbn());
         bookDataFormData.setYearOfPublication(String.valueOf(TEST_BOOK.getYearOfPublication()));
+    }
+
+    private void setupFormDataWithDescription() {
+        bookDataFormData.setTitle(TEST_BOOK_WITH_DESCRIPTION.getTitle());
+        bookDataFormData.setAuthor(TEST_BOOK_WITH_DESCRIPTION.getAuthor());
+        bookDataFormData.setEdition(TEST_BOOK_WITH_DESCRIPTION.getEdition());
+        bookDataFormData.setIsbn(TEST_BOOK_WITH_DESCRIPTION.getIsbn());
+        bookDataFormData.setDescription(TEST_BOOK_WITH_DESCRIPTION.getDescription());
+        bookDataFormData.setYearOfPublication(String.valueOf(TEST_BOOK_WITH_DESCRIPTION.getYearOfPublication()));
     }
 }
